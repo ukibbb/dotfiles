fc931846b58f3a0073a0dbdf1b3cbe73ba401809
Lukasz Bulinski <lucas.bulinski@gmail.com>
2023-09-11 18:21:53 +0200

    helloworld

--- nvim/lua/uki/plugins/bufferline.lua
+++ nvim/lua/uki/plugins/bufferline.lua
@@ -4,7 +4,6 @@ return {
 	version = "*",
 	opts = {
 		options = {
-			mode = "tabs",
 			separator_style = "slant",
 		},
 	},
--- nvim/lua/uki/plugins/colorscheme.lua
+++ nvim/lua/uki/plugins/colorscheme.lua
@@ -1,7 +1,7 @@
 return {
-	"folke/tokyonight.nvim",
+	"navarasu/onedark.nvim",
 	priority = 1000,
 	config = function()
-		vim.cmd([[colorscheme tokyonight-moon ]])
+		vim.cmd([[colorscheme onedark ]])
 	end,
 }
