4da0d3304d5e1c36276c30f2b06da62ab09a87fa
Lukasz Bulinski <lucas.bulinski@gmail.com>
2023-10-14 11:07:38 +0200

    current

--- nvim/lua/uki/core/options.lua
+++ nvim/lua/uki/core/options.lua
@@ -1,17 +1,27 @@
 local options = vim.opt
 
+options.relativenumber = true
+options.number = true
+
 -- set termguicolors to enable highlight groups
 options.termguicolors = true
 
 options.tabstop = 2
 options.shiftwidth = 2
+-- options.expandtab = true
+-- options.autoindent = true
+
+options.smartindent = true
+options.wrap = true
 
 options.clipboard:append("unnamedplus")
 
-options.ignorecase = true
-options.smartcase = true
+options.ignorecase = true -- ignore case when searching
+options.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
 
-options.relativenumber = true
-options.number = true
+options.swapfile = false
+options.backup = false
+
+-- options.scrolloff = 8
 
 vim.cmd([[autocmd FileType * set formatoptions-=cro]])
